355682483c7daada97254fd9be8a1fde63e48ef3
KeithWilliamsOutback <keith@inlanddistributors.com.au>
2026-04-29 08:38:43 +1000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // VYND Driver App — Service Worker
 // ── BUMP THIS VERSION NUMBER every time you push to GitHub ──
-const VERSION = 'vynd-driver-v1.0.0';
+const VERSION = 'vynd-driver-v1.0.1';
 
 const CACHE = VERSION;
 
